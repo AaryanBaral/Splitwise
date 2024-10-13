a8f89bd9f58b1087f622e7a32e9f04b8d6d007ad
Aaryan Baral <aryanbaral1100@gmail.com>
2024-10-14 00:08:08 +0545

    Group Creation code completed

--- Models/GroupMembers.cs
+++ Models/GroupMembers.cs
@@ -6,9 +6,9 @@ namespace Splitwise_Back.Models
     public class GroupMembers
     {
         public required int GroupId { get; set; }
-        public  Groups Group { get; set; } // Navigation Property to Groups Table
+        public Groups? Group { get; set; } // Navigation Property to Groups Table
         public required string UserId { get; set; }
-        public  CustomUser User { get; set; } // Navigation Property to Users Table
+        public  CustomUser? User { get; set; } // Navigation Property to Users Table
         public required bool IsAdmin { get; set; } // Navigation Property to Users Table
         public required DateTime JoinDate { get; set; } = DateTime.UtcNow;
     }
