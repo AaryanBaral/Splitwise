ac73f7544b1ed60e4f3061af7b6ff0157c00e534
Aaryan Baral <aryanbaral1100@gmail.com>
2024-10-19 19:45:25 +0545

    ok

--- Models/ExpenseShare.cs
+++ Models/ExpenseShare.cs
@@ -10,7 +10,7 @@ namespace Splitwise_Back.Models
         public required string UserId { get; set; } // Foreign Key to Users Table
         public required CustomUser User { get; set; } // Navigation Property for User who owes a portion
         public required string OwesUserId { get; set; }  // Foreign Key to User table (the one who is owed money)
-        public required CustomUser OwesUser { get; set; }  // Navigation property
+        public required CustomUser OwesUser { get; set; }  // Navigation property to the person who is owed
 
         public required decimal AmountOwed { get; set; } // Amount the user owes for this expense
         public required string  ShareType { get; set; } // e.g., "Equal", "Percentage"
